b3d4b1196410194b42f84daf9f654df638d4f404
Scott Moon <interactord@gmail.com>
2025-04-03 14:25:32 +0900

    feat: include VisionOS Platform

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 
 let package = Package(
   name: "rtt-sdk",
+  platforms: [.iOS(.v17), .visionOS(.v1)],
   products: [
     .library(
       name: "rtt-sdk",
@@ -17,10 +18,5 @@ let package = Package(
       name: "LiveTranslationSDK_iOS",
       path: "iOS/binary/LiveTranslationSDK_iOS.xcframework"
     ),
-//    .binaryTarget(
-//      name: "LiveTranslationSDK_iOS",
-//      url: "https://github.com/flitto/rtt_sdk/blob/main/ios/binary/LiveTranslationSDK_iOS.xcframework",
-//      checksum: "0566a694dc3ad4c8f5a0f30f149dec4d4edf14af6c7d5e3d11dafbd18999f12c")
-
   ]
 )
