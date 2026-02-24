52e77707194a0871745c774db4e0561cc86356f9
kyungseok.lee <lks574@naver.com>
2026-02-24 12:56:38 +0900

    refactor(demo): initialize LiveTranslationSDK at app launch

--- ios/LT_Demo/LT_Demo/LT_DemoApp.swift
+++ ios/LT_Demo/LT_Demo/LT_DemoApp.swift
@@ -1,7 +1,12 @@
+import LiveTranslationSDK_iOS
 import SwiftUI
 
 @main
 struct LT_DemoApp: App {
+  init() {
+    LiveTranslationSDK.configure(environment: .development)
+  }
+
   var body: some Scene {
     WindowGroup {
       ContentView()
